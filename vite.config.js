32d72e23aa3c8d0ab16581482433db5a4804434e
AdelinePat <adeline.patenne@hotmail.fr>
2025-10-26 12:24:57 +0100

    feat: new entrypoint for vite in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
 
 export default defineConfig({
-  root: "src",
-  buid: {
-    outDir: "../dist",
+  root: './src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        form: resolve(__dirname, 'src/form/form.html'),
+      }
+    }
   },
 });
